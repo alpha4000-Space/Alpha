cdac777233f85eec1e0fc61a505202f73233544f
alpha4000-Space <alphachange4000@gmail.com>
2026-04-23 07:19:03 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 BOT_TOKEN = "8309649950:AAGeDL4ZG7ChNCfKLpRbAeyRh_l8vwGkh6Y"
-ADMIN_IDS = []  # o'zgartiring
+ADMIN_IDS = [7399101034]  # o'zgartiring
 REQUIRED_CHANNELS = []
 DB_FILE = "database.json"
